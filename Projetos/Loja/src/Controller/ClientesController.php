ac75d6c215dcdd6fe24bbf4c8b1a15f1fb295d13
otacilio <otaciliofilho289@gmail.com>
2021-08-08 21:15:43 -0300

    Projeto escola

---
+++ Projetos/Loja/src/Controller/ClientesController.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Clientes Controller
+ *
+ * @property \App\Model\Table\ClientesTable $Clientes
+ * @method \App\Model\Entity\Cliente[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ClientesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Produtos', 'Categorias'],
+        ];
+        $clientes = $this->paginate($this->Clientes);
+
+        $this->set(compact('clientes'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Cliente id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $cliente = $this->Clientes->get($id, [
+            'contain' => ['Produtos', 'Categorias'],
+        ]);
+
+        $this->set(compact('cliente'));
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $cliente = $this->Clientes->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $cliente = $this->Clientes->patchEntity($cliente, $this->request->getData());
+            if ($this->Clientes->save($cliente)) {
+                $this->Flash->success(__('The cliente has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The cliente could not be saved. Please, try again.'));
+        }
+        $produtos = $this->Clientes->Produtos->find('list', ['limit' => 200]);
+        $categorias = $this->Clientes->Categorias->find('list', ['limit' => 200]);
+        $this->set(compact('cliente', 'produtos', 'categorias'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Cliente id.
+     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $cliente = $this->Clientes->get($id, [
+            'contain' => [],
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $cliente = $this->Clientes->patchEntity($cliente, $this->request->getData());
+            if ($this->Clientes->save($cliente)) {
+                $this->Flash->success(__('The cliente has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The cliente could not be saved. Please, try again.'));
+        }
+        $produtos = $this->Clientes->Produtos->find('list', ['limit' => 200]);
+        $categorias = $this->Clientes->Categorias->find('list', ['limit' => 200]);
+        $this->set(compact('cliente', 'produtos', 'categorias'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Cliente id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $cliente = $this->Clientes->get($id);
+        if ($this->Clientes->delete($cliente)) {
+            $this->Flash->success(__('The cliente has been deleted.'));
+        } else {
+            $this->Flash->error(__('The cliente could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ Projetos/Loja/tests/Fixture/ClientesFixture.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ClientesFixture
+ */
+class ClientesFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // phpcs:disable
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 10, 'autoIncrement' => true, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null, 'unsigned' => null],
+        'nome' => ['type' => 'string', 'length' => 50, 'default' => null, 'null' => false, 'collate' => null, 'comment' => null, 'precision' => null],
+        'produto_id' => ['type' => 'biginteger', 'length' => 20, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null, 'unsigned' => null, 'autoIncrement' => null],
+        'categoria_id' => ['type' => 'biginteger', 'length' => 20, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null, 'unsigned' => null, 'autoIncrement' => null],
+        'created' => ['type' => 'date', 'length' => null, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null],
+        'modified' => ['type' => 'date', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+    ];
+    // phpcs:enable
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'nome' => 'Lorem ipsum dolor sit amet',
+                'produto_id' => 1,
+                'categoria_id' => 1,
+                'created' => '2021-08-04',
+                'modified' => '2021-08-04',
+            ],
+        ];
+        parent::init();
+    }
+}
